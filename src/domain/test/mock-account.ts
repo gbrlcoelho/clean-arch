002bc155748075d81de3e20294260a25f4eed965
gbrlcoelho <coelhogabriel67@gmail.com>
2022-12-07 14:15:37 -0300

    refactor: group imports in index files

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,6 +1,6 @@
-import { AuthenticationParams } from '@/domain/usecases/authentication'
+import { AuthenticationParams } from '@/domain/usecases'
 import { faker } from '@faker-js/faker'
-import { AccountModel } from '../models/account-model'
+import { AccountModel } from '../models'
 
 export const mockAuthentication = (): AuthenticationParams => ({
   email: faker.internet.email(),
