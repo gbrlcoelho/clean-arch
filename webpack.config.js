71d2c51c02fc1049134569ab541aede3f52e53b4
gbrlcoelho <coelhogabriel67@gmail.com>
2022-12-08 17:35:02 -0300

    chore: config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-
+const path = require('path')
+ 
 module.exports = {
   mode: 'development',
   entry: './src/main/index.tsx',
@@ -18,29 +18,32 @@ module.exports = {
   module: {
     rules: [{
       test: /\.ts(x?)$/,
-      loader: 'tsloader',
+      loader: 'ts-loader',
       exclude: /node_modules/
-    },
-    {
+    }, {
       test: /\.scss$/,
       use: [{
         loader: 'style-loader'
-      },
-      {
+      }, {
         loader: 'css-loader',
         options: {
           modules: true
         }
-      },
-      {
+      }, {
         loader: 'sass-loader'
       }]
     }]
   },
   devServer: {
-    contentBase: './public',
-    writeToDisk: true,
-    historyApiFallback: true
+    static: './public',
+    devMiddleware: {
+      writeToDisk: true,
+    },
+    historyApiFallback:{
+      rewrites: [
+        { from: /./, to: '/index.html' }
+      ]
+  },
   },
   externals: {
     react: 'React',
