a898a90e507d22bdf2f33b18414d362a07acc611
gbrlcoelho <coelhogabriel67@gmail.com>
2023-01-03 10:44:46 -0300

    fix: ensure email validation allows empty value

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -11,9 +11,15 @@ describe('EmailValidation', () => {
     expect(error).toEqual(new InvalidFieldError())
   })
 
-  test('Should return error if email is invalid', () => {
+  test('Should return falsy if email is valid', () => {
     const sut = makeSut()
     const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
+  })
+
+  test('Should return falsy if email is empty', () => {
+    const sut = makeSut()
+    const error = sut.validate('')
     expect(error).toBeFalsy()
   })
 })
